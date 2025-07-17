8e553abc2d4c4d23789e0999b1207499b004c5ee
Fatih Sinan Celik <facelik@k1m19s02.42istanbul.com.tr>
2025-07-17 21:13:11 +0300

    str is done

---
+++ ft_putstr.c
@@ -0,0 +1,20 @@
+
+#include "ft_printf.h"
+
+int	ft_putchar(int c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	return (i);
+}
