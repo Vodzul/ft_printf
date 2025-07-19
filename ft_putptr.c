51c7a2af2b0fe96517a0466b5fcd893bb1b833b1
Vodzu <fatih2005sinan@gmail.com>
2025-07-20 01:56:17 +0300

    printf is done but not tested

--- ft_putptr.c
+++ ft_putptr.c
@@ -3,11 +3,13 @@
 
 int	ft_putptr(unsigned long ptr)
 {
-	int	count;
+	static int	count = 0;
 
-	count = 0;
 	if (ptr == 0)
-		return (count += write(1, "(nil)", 5));
+	{
+		count += write(1, "(nil)", 5);
+		return (count);
+	}
 	count += write(1, "0x", 2);
 	count += ft_puthex(ptr, 'x');
 	return (count);
